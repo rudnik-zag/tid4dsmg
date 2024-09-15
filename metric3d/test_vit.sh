6f9c2306b2e7ef499219af2e26020636aed2df68
rudnik-zag <rudnikzagradje@gmail.com>
2024-09-15 18:04:32 +0200

    gaussian renderer

--- metric3d/test_vit.sh
+++ metric3d/test_vit.sh
@@ -1,4 +1,4 @@
-python mono/tools/test_scale_cano.py \
+python ./mono/tools/test_scale_cano.py \
     'mono/configs/HourglassDecoder/vit.raft5.small.py' \
     --load-from ./weight/metric_depth_vit_small_800k.pth \
     --test_data_path ./data/wild_demo \
