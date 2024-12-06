a31e20c57ad1bf06d91e3445eb54c60eaacce281
rudnik-zag <rudnikzagradje@gmail.com>
2024-12-06 22:19:02 +0100

    remove marigold and metric3d from main folder to external.

--- metric3d/test_vit.sh
+++
@@ -1,5 +0,0 @@
-python ./mono/tools/test_scale_cano.py \
-    'mono/configs/HourglassDecoder/vit.raft5.giant2.py' \
-    --load-from ./weight/metric_depth_vit_giant2_800k.pth \
-    --test_data_path ./data/colmap_data \
-    --launcher None
